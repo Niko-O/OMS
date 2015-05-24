9638874f93d20f12d5a87046e2872fd648b2d4e1
NameLess-- <NameLess--@users.noreply.github.com>
2015-05-24 21:00:58 +0200

    Default Tennis Template hinzugefügt
    Dies ist das Standard-Tennis Template, das mitgeliefert wird.
    Weitere Funktionen folgen.

---
+++ templates_tennis/js/tennis.js
@@ -0,0 +1,50 @@
+function showScoreboard()
+{
+    document.getElementById("scoreboard").style.visibility = "visible";
+}
+
+function hideScoreboard()
+{
+    document.getElementById("scoreboard").style.visibility = "hidden";
+}
+
+function setTeamNameOne(name)
+{
+    document.getElementById("name_one").innerHTML = name;
+}
+
+function setTeamNameTwo(name)
+{
+    document.getElementById("name_two").innerHTML = name;
+}
+
+function setPointsOne(points)
+{
+    document.getElementById("points_one").innerHTML = points;
+}
+
+function setPointsTwo(points)
+{
+    document.getElementById("points_two").innerHTML = points;
+}
+
+function setGamesOne(games)
+{
+    document.getElementById("games_one").innerHTML = games;
+}
+
+function setGamesTwo(games)
+{
+    document.getElementById("games_two").innerHTML = games;
+}
+
+function setSetsOne(sets)
+{
+    document.getElementById("sets_one").innerHTML = sets;
+}
+
+function setSetsTwo(sets)
+{
+    document.getElementById("sets_two").innerHTML = sets;
+}
+
