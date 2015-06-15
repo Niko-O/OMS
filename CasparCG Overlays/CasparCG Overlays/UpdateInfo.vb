dd0c68b39a60fff06160b94b678ac2c318ca9e6d
Niko-O <niko.o.generic@gmx.at>
2015-06-15 06:23:50 +0200

    Diverses aufgeräumt
    CasparServer ICommandResponse Name geändert + Exception-Message gefixt-
    UpdateInfo Public Key als XML-Literal angegeben.
    DefaultTennisTemplate entfernt.

--- CasparCG Overlays/CasparCG Overlays/UpdateInfo.vb
+++ CasparCG Overlays/CasparCG Overlays/UpdateInfo.vb
@@ -39,7 +39,15 @@ Public Class UpdateInfo
         _UpdateManager = New updateSystemDotNet.updateController
         _UpdateManager.updateUrl = "http://baellchen.hol.es/ccg"
         _UpdateManager.projectId = "89d28181-2325-4eb1-8006-d72aa52ee34c"
-        _UpdateManager.publicKey = "<RSAKeyValue><Modulus>ssmUWkhjJzneeNZmDuw71cuB1RjYzIU/SO7MtmCxpTwoUhHTSyvQOjmBhBtj2nixgmg6no7ymHFwmNSoY67o2+aBOZd29nCq2KtpQqQ+cw7ONg0DkgzSHyhhjQGXxNEO7/GciArikb0RZA5ekmuXoxspcw6dzF+xH4y2tlcwNVDiS+0sFc8wYnnrvNZh+eH4U3ceW6dQJjC6RV3Qy0Zj15ZF0cpauZlG1z7wna8t9RHQP8fsHdEoxjLhi367xlHD4eTGS18j1wAmbTNul6Abfw/gONk7zbTifyLkxkqK76BmjMZaKUjMz2cC+1f8bbPEt0ArhT/cpbGzsdJwAJPEsIG+rZsaV9lr8cm5GGe2YI92g38O1Vce/lMG7RFQ6OPKyPwixBFvjXo2Fw98W9ev1iGhjHk5D6VJQ+lf0/Imwos8PDrRDEjMJFMhqEIWouppwKMH+d1Xopsl27gTnHcqYu2Z+tE0WvsVyUlK8/1AoTGtJxIlz/u+T7sEKrTeX2RPqgstUXUkih9Oe60LO+gQlHXbH8P5iZK0Kt4w0FnM9Q4qwPwN6HEhRNxsyE2xdzZnNVXdcb8bw7T5tcRy/nwlcEVARu13tsmFkRLxcpdsOJb3EoHgErBXwJknQR/Gu4i35UxqClRFrxPWfGM7ed3sqgMlk0QiaP86Q5pHUovf1qk=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>"
+        _UpdateManager.publicKey = _
+            <RSAKeyValue>
+                <Modulus>
+                    ssmUWkhjJzneeNZmDuw71cuB1RjYzIU/SO7MtmCxpTwoUhHTSyvQOjmBhBtj2nixgmg6no7ymHFwmNSoY67o2+aBOZd29nCq2KtpQqQ+cw7ONg0DkgzSHyhhjQGXxNEO7/GciArikb0RZA5ekmuXoxspcw6dzF+xH4y2tlcwNVDiS+0sFc8wYnnrvNZh+eH4U3ceW6dQJjC6RV3Qy0Zj15ZF0cpauZlG1z7wna8t9RHQP8fsHdEoxjLhi367xlHD4eTGS18j1wAmbTNul6Abfw/gONk7zbTifyLkxkqK76BmjMZaKUjMz2cC+1f8bbPEt0ArhT/cpbGzsdJwAJPEsIG+rZsaV9lr8cm5GGe2YI92g38O1Vce/lMG7RFQ6OPKyPwixBFvjXo2Fw98W9ev1iGhjHk5D6VJQ+lf0/Imwos8PDrRDEjMJFMhqEIWouppwKMH+d1Xopsl27gTnHcqYu2Z+tE0WvsVyUlK8/1AoTGtJxIlz/u+T7sEKrTeX2RPqgstUXUkih9Oe60LO+gQlHXbH8P5iZK0Kt4w0FnM9Q4qwPwN6HEhRNxsyE2xdzZnNVXdcb8bw7T5tcRy/nwlcEVARu13tsmFkRLxcpdsOJb3EoHgErBXwJknQR/Gu4i35UxqClRFrxPWfGM7ed3sqgMlk0QiaP86Q5pHUovf1qk=
+                </Modulus>
+                <Exponent>
+                    AQAB
+                </Exponent>
+            </RSAKeyValue>.ToString
         _UpdateManager.releaseInfo.Version = CurrentApplicationVersion.ToString
         _UpdateManager.releaseFilter.checkForFinal = True
         _UpdateManager.releaseFilter.checkForBeta = False
